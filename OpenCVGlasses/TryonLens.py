ab0f908909a4c712bcf32edf0828cea0def137d6
Araav Nayak <104335810+AraavNayak@users.noreply.github.com>
2023-04-09 07:06:45 -0700

    Add files via upload

---
+++ OpenCVGlasses/TryonLens.py
@@ -0,0 +1,33 @@
+import cv2
+import cvzone
+import keyboard
+#import _tkinter
+
+print("Welcome to VisionARy's virtual AR glasses simulator.\nThis simulator enables users to try out various glasses in real time.\nPress 'd' to see the next glasses in the list and 's' to see the previous glasses in the list\n")
+cap = cv2.VideoCapture(0)
+cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+num=1
+
+while True:
+    k=cv2.waitKey(1)
+    if k==ord('d'):
+        num+=1
+    if k==ord('s'):
+        if(num > 1):
+            num-=1
+    if(num<=29 & num >= 0):
+            overlay = cv2.imread('Glasses/glass{}.png'.format(num), cv2.IMREAD_UNCHANGED)
+         
+    _, frame = cap.read()
+    gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(gray_scale)
+    for (x, y, w, h) in faces:
+        #cv2.rectangle(frame,(x, y), (x+w, y+h), (0, 255, 0), 2)
+        overlay_resize = cv2.resize(overlay,(w,int(h*0.7)))
+        frame = cvzone.overlayPNG(frame, overlay_resize, [x, y])
+    cv2.imshow('SnapLens', frame)
+    if cv2.waitKey(10) == ord('q') or num>29:
+        break
+  
+cap.release()
+cv2.destroyAllWindows()
