ef62a71be58763440cc22f67e869af756040e2b4
Araav Nayak <104335810+AraavNayak@users.noreply.github.com>
2023-07-21 21:33:59 -0700

    Update TryonLens.py

--- OpenCVGlasses/TryonLens.py
+++ OpenCVGlasses/TryonLens.py
@@ -3,19 +3,25 @@ import cvzone
 import keyboard
 #import _tkinter
 
-print("Welcome to VisionARy's virtual AR glasses simulator.\nThis simulator enables users to try out various glasses in real time.\nPress 'd' to see the next glasses in the list and 's' to see the previous glasses in the list\n")
+print("Welcome to VisionARy's virtual AR glasses simulator.\nThis simulator enables users to try out various glasses in real time.\nPress 'd' to see the next glasses in the list and 's' to see the previous glasses in the list, and press 'q' to quit.\n")
 cap = cv2.VideoCapture(0)
 cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 num=1
+numGlasses = 29
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+#cv2.putText('Christmas', (10,50), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
 
 while True:
     k=cv2.waitKey(1)
     if k==ord('d'):
-        num+=1
+        if num < numGlasses:
+            num+=1
     if k==ord('s'):
         if(num > 1):
             num-=1
-    if(num<=29 & num >= 0):
+
+    if(num<=29 and num >= 0):
             overlay = cv2.imread('Glasses/glass{}.png'.format(num), cv2.IMREAD_UNCHANGED)
          
     _, frame = cap.read()
@@ -25,7 +31,8 @@ while True:
         #cv2.rectangle(frame,(x, y), (x+w, y+h), (0, 255, 0), 2)
         overlay_resize = cv2.resize(overlay,(w,int(h*0.7)))
         frame = cvzone.overlayPNG(frame, overlay_resize, [x, y])
-    cv2.imshow('SnapLens', frame)
+    cv2.putText(frame, 'Displaying ' + str(num) + '/' + str(numGlasses), (50, 100), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.imshow('VisionARy', frame)
     if cv2.waitKey(10) == ord('q') or num>29:
         break
   
